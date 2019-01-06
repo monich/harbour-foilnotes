847a83fcd55e6bfc5a7e9962cc172483bcb960ed
Nokius <mail@nokius.net>
2019-01-06 20:17:26 +0100

    German translation

---
+++ translations/harbour-foilnotes-de.ts
@@ -0,0 +1,310 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de">
+<context>
+    <name></name>
+    <message id="foilnotes-app_name">
+        <source>Foil Notes</source>
+        <extracomment>Application title</extracomment>
+        <translation>Foil Notes</translation>
+    </message>
+    <message id="foilnotes-notification-copied_to_clipboard">
+        <source>Copied to clipboard</source>
+        <extracomment>Pop-up notification</extracomment>
+        <translation>Kopiert in Zwischenablage</translation>
+    </message>
+    <message id="foilnotes-plaintext_view-placeholder">
+        <source>You do not have any notes.</source>
+        <extracomment>Placeholder text</extracomment>
+        <translation>Du hast keine Notizen</translation>
+    </message>
+    <message id="foilnotes-encrypted_view-placeholder">
+        <source>You do not have any encrypted notes</source>
+        <extracomment>Placeholder text</extracomment>
+        <translation>Du hast keine verschlüsselten Notizen</translation>
+    </message>
+    <message id="foilnotes-plaintext_view-placeholder_hint">
+        <source>Open pulley menu to add one.</source>
+        <extracomment>Placeholder hint</extracomment>
+        <translation>Nutze das pulley Menu um eine Notiz zuerstllen</translation>
+    </message>
+    <message id="foilnotes-menu-new_note">
+        <source>New note</source>
+        <extracomment>Create a new note ready for editing</extracomment>
+        <translation>Neue Notiz</translation>
+    </message>
+    <message id="foilnotes-menu-new_secret_note">
+        <source>New secret note</source>
+        <extracomment>Create a new secret note ready for editing</extracomment>
+        <translation>Neue verschlüsselte Notiz</translation>
+    </message>
+    <message id="foilnotes-menu-select_color">
+        <source>Select color</source>
+        <extracomment>Select note color</extracomment>
+        <translation>Wähle eine Farbe</translation>
+    </message>
+    <message id="foilnotes-menu-delete_note">
+        <source>Delete note</source>
+        <extracomment>Delete this note from note page</extracomment>
+        <translation>Löschen der Notiz</translation>
+    </message>
+    <message id="foilnotes-menu-encrypt">
+        <source>Encrypt</source>
+        <extracomment>Generic menu item</extracomment>
+        <translation>Entschlüsselt</translation>
+    </message>
+    <message id="foilnotes-menu-decrypt">
+        <source>Decrypt</source>
+        <extracomment>Generic menu item</extracomment>
+        <translation>Verschlüsselt</translation>
+    </message>
+    <message id="foilnotes-menu-delete">
+        <source>Delete</source>
+        <extracomment>Generic menu item</extracomment>
+        <translation>Löschen</translation>
+    </message>
+    <message id="foilnotes-menu-copy">
+        <source>Copy to clipboard</source>
+        <extracomment>Context menu item</extracomment>
+        <translation>Kopiere in die Zwischenablage</translation>
+    </message>
+    <message id="foilnotes-menu-change_password">
+        <source>Change password</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Passwort ändern</translation>
+    </message>
+    <message id="foilnotes-remorse-deleting">
+        <source>Deleting</source>
+        <extracomment>Remorse item text, will delete note when timer expires</extracomment>
+        <translation>Löschen</translation>
+    </message>
+    <message id="foilnotes-default_file_name">
+        <source>note</source>
+        <extracomment>Placeholder name for note filename</extracomment>
+        <translation>Notiz</translation>
+    </message>
+    <message id="foilnotes-placeholder-empty_note">
+        <source>Write a note...</source>
+        <extracomment>Placeholder text for new notes. At this point there is nothing else on the screen.</extracomment>
+        <translation>Schreibe eine Notiz</translation>
+    </message>
+    <message id="foilnotes-menu-organize">
+        <source>Organize</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Organisieren</translation>
+    </message>
+    <message id="foilnotes-menu-select">
+        <source>Select</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Wähle</translation>
+    </message>
+    <message id="foilnotes-menu-select_all">
+        <source>Select all</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Alle auswählen</translation>
+    </message>
+    <message id="foilnotes-menu-select_none">
+        <source>Clear all</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Lösche alles</translation>
+    </message>
+    <message id="foilnotes-remorse-deleting_selected" numerus="yes">
+        <source>Deleting %0 selected notes</source>
+        <extracomment>Generic remorse popup text</extracomment>
+        <translation>
+            <numerusform>Löschen %0 der Notiz</numerusform>
+            <numerusform>Löschen %0 der Notiz</numerusform>
+        </translation>
+    </message>
+    <message id="foilnotes-organize_page-plaintext_header">
+        <source>Organize notes</source>
+        <extracomment>Page header</extracomment>
+        <translation>Organisieren der Notizen</translation>
+    </message>
+    <message id="foilnotes-organize_page-secret_header">
+        <source>Organize secret notes</source>
+        <extracomment>Page header</extracomment>
+        <translation>Organisieren der verschlüsselten Notizen</translation>
+    </message>
+    <message id="foilnotes-select_page-header">
+        <source>Select notes</source>
+        <extracomment>Page header</extracomment>
+        <translation>Wähle Notiz</translation>
+    </message>
+    <message id="foilnotes-notification-generated_key">
+        <source>Generated new key</source>
+        <extracomment>Pop-up notification</extracomment>
+        <translation>Generieren eines neuen Schlüssles</translation>
+    </message>
+    <message id="foilnotes-notification-password_changed">
+        <source>Password changed</source>
+        <extracomment>Pop-up notification</extracomment>
+        <translation>Password ändern</translation>
+    </message>
+    <message id="foilnotes-enter_password_view-label-enter_password">
+        <source>Secret notes are locked. Please enter your password</source>
+        <extracomment>Password prompt label</extracomment>
+        <translation>Ausgewählte Notizen sind verschlüsselt. Gib dein Passwort bitte ein</translation>
+    </message>
+    <message id="foilnotes-enter_password_view-button-unlocking">
+        <source>Unlocking...</source>
+        <extracomment>Button label</extracomment>
+        <translation>Öffnen...</translation>
+    </message>
+    <message id="foilnotes-enter_password_view-button-unlock">
+        <source>Unlock</source>
+        <extracomment>Button label</extracomment>
+        <translation>Öffnen</translation>
+    </message>
+    <message id="foilnotes-menu-generate_key">
+        <source>Generate a new key</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Generieren eines neuen Schlüssles</translation>
+    </message>
+    <message id="foilnotes-decrypting_view-unlocking">
+        <source>Unlocking...</source>
+        <extracomment>Progress view label</extracomment>
+        <translation>Öffnen...</translation>
+    </message>
+    <message id="foilnotes-decrypting_view-decrypting">
+        <source>Decrypting...</source>
+        <extracomment>Progress view label</extracomment>
+        <translation>Entschlüsseln...</translation>
+    </message>
+    <message id="foilnotes-generate_key_page-title">
+        <source>You are about to generate a new key</source>
+        <extracomment>Label text</extracomment>
+        <translation>Du bist dabei einen neuen Schlüssel zu erstellen</translation>
+    </message>
+    <message id="foilnotes-generate_key_view-label-key_needed">
+        <source>You need to generate the key and select the password before you can encrypt your notes</source>
+        <extracomment>Label text</extracomment>
+        <translation>Du musst einen Schlüssel erstellen und ein Passwort wählen, bevor du Notizen verschlüsseln kannst.</translation>
+    </message>
+    <message id="foilnotes-generate_key_view-label-key_size">
+        <source>Key size</source>
+        <extracomment>Combo box label</extracomment>
+        <translation>Schlüsselgröße</translation>
+    </message>
+    <message id="foilnotes-generate_key_view-label-minimum_length" numerus="yes">
+        <source>Type at least %0 character(s)</source>
+        <extracomment>Password field label</extracomment>
+        <translation>Nutze mindestens %0 Zeichen</translation>
+    </message>
+    <message id="foilnotes-generate_key_view-button-generate_key">
+        <source>Generate key</source>
+        <extracomment>Button label</extracomment>
+        <translation>Schlüssel generieren</translation>
+    </message>
+    <message id="foilnotes-generate_key_view-button-generating_key">
+        <source>Generating...</source>
+        <extracomment>Button label</extracomment>
+        <translation>Generieren...</translation>
+    </message>
+    <message id="foilnotes-generate_key_warning-title">
+        <source>Warning</source>
+        <extracomment>Title for the new key warning</extracomment>
+        <translation>Warnung</translation>
+    </message>
+    <message id="foilnotes-generate_key_warning-text">
+        <source>You seem to have some encrypted files in the storage folder. Once you have generated a new key, you are going to lose access to those files. If you have forgotten your password, then keep in mind that most likely it&apos;s computationally easier to brute-force your password and recover the old key than to crack the new key.</source>
+        <extracomment>Warning shown prior to generating the new key</extracomment>
+        <translation>Du hast verschlüsselte Datein im Ordner. Wenn du einen neuen Schlüssel erstellst wirst du den Zugriff auf diese Daten verlieren. Wenn du dein Passwort vergessen hast, wird es einfacher sein dein Passwort mittles Brute-Force wiederherstellen, als den neuen Schlüssel zu cracken. </translation>
+    </message>
+    <message id="foilnotes-generating_key_view-generating_new_key">
+        <source>Generating new key...</source>
+        <extracomment>Progress view label</extracomment>
+        <translation>Neuen Schlüssel generieren...</translation>
+    </message>
+    <message id="foilnotes-change_password_page-label-enter_passwords">
+        <source>Please enter the current and the new password</source>
+        <extracomment>Password change prompt</extracomment>
+        <translation>Gib dein altes und neues Passwort ein</translation>
+    </message>
+    <message id="foilnotes-change_password_page-text_field_label-current_password">
+        <source>Current password</source>
+        <extracomment>Placeholder and label for the current password prompt</extracomment>
+        <translation>Aktualne hasło</translation>
+    </message>
+    <message id="foilnotes-change_password_page-text_field_label-new_password">
+        <source>New password</source>
+        <extracomment>Placeholder and label for the new password prompt</extracomment>
+        <translation>Neues Passwort</translation>
+    </message>
+    <message id="foilnotes-change_password_page-button-change_password">
+        <source>Change password</source>
+        <extracomment>Button label</extracomment>
+        <translation>Passwort ändern</translation>
+    </message>
+    <message id="foilnotes-confirm_password_page-info_label">
+        <source>Please type in your new password one more time</source>
+        <extracomment>Password confirmation label</extracomment>
+        <translation>Wiederhole dein neues Passwort</translation>
+    </message>
+    <message id="foilnotes-confirm_password_page-description">
+        <source>Make sure you don&apos;t forget your password. It&apos;s impossible to either recover it or to access the encrypted notes without knowing it. Better take it seriously.</source>
+        <extracomment>Password confirmation description</extracomment>
+        <translation>Beachte, wen du dein Password vergisst hast du kein Zugriff mehr auf deine veschlüsselten Notizen.</translation>
+    </message>
+    <message id="foilnotes-confirm_password_page-text_field_placeholder-new_password">
+        <source>New password again</source>
+        <extracomment>Placeholder for the password confirmation prompt</extracomment>
+        <translation>Wiederhole dein neues Password</translation>
+    </message>
+    <message id="foilnotes-confirm_password_page-text_field_label-new_password">
+        <source>New password</source>
+        <extracomment>Label for the password confirmation prompt</extracomment>
+        <translation>Neues Passwort</translation>
+    </message>
+    <message id="foilnotes-confirm_password_page-button-confirm">
+        <source>Confirm</source>
+        <extracomment>Button label (confirm password)</extracomment>
+        <translation>Bestätigen</translation>
+    </message>
+    <message id="foilnotes-remorse-decrypting">
+        <source>Decrypting</source>
+        <extracomment>Decrypting note in 5 seconds</extracomment>
+        <translation>Entschlüsseln</translation>
+    </message>
+    <message id="foilnotes-remorse-decrypting_selected" numerus="yes">
+        <source>Decrypting %0 selected notes</source>
+        <extracomment>Generic remorse popup text</extracomment>
+        <translation>Entschlüssel %0 ausgewählte Notizen</translation>
+    </message>
+    <message id="foilnotes-hint-delete_selected">
+        <source>Delete selected notes</source>
+        <extracomment>Hint text</extracomment>
+        <translation>Lösche ausgewählte Notizen</translation>
+    </message>
+    <message id="foilnotes-hint-encrypt_selected">
+        <source>Encrypt selected notes</source>
+        <extracomment>Hint text</extracomment>
+        <translation>Verschlüssel ausgewählte Notizen</translation>
+    </message>
+    <message id="foilnotes-hint-decrypt_selected">
+        <source>Decrypt selected notes</source>
+        <extracomment>Hint text</extracomment>
+        <translation>Entschlüssel ausgewählte Notizen</translation>
+    </message>
+    <message id="foilnotes-foil_pics_warning">
+        <source>Note that Foil Notes and Foil Pics share the encryption key and the password.</source>
+        <extracomment>Warning text, small size label below the password prompt</extracomment>
+        <translation>Achtung, Foil Notes und Foil Pics nutzen beide den gleichen Schlüssel und das gleiche Passwort</translation>
+    </message>
+    <message id="foilnotes-hint-swipe_left_to_plaintext">
+        <source>Swipe left to access plaintext notes</source>
+        <extracomment>Left swipe hint text</extracomment>
+        <translation>Wische nach Links um deine unverschlüsselten Notizen zusehen</translation>
+    </message>
+    <message id="foilnotes-hint-swipe_right_to_encrypted">
+        <source>Encrypted pictures are moved there to the left</source>
+        <extracomment>Right swipe hint text</extracomment>
+        <translation>Wische nach Rechts um deine verschlüsselten Notizen zusehen</translation>
+    </message>
+    <message id="foilnotes-hint-swipe_left_to_decrypted">
+        <source>Decrypted notes are moved back to the right</source>
+        <extracomment>Left swipe hint text</extracomment>
+        <translation>Entschlüsselte Notizen werden nach rechts bewegt</translation>
+    </message>
+</context>
+</TS>

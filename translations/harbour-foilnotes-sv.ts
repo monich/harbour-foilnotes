14e4aa7a44b2d0df84d01ebadd3f337036f8ed7b
Åke Engelbrektson <eson57@users.noreply.github.com>
2019-01-06 16:47:53 +0100

    Create harbour-foilnotes-sv.ts
    Thanks! I've been waiting for this. 
    Here's Swedish translation.

---
+++ translations/harbour-foilnotes-sv.ts
@@ -0,0 +1,316 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name></name>
+    <message id="foilnotes-app_name">
+        <source>Foil Notes</source>
+        <extracomment>Application title</extracomment>
+        <translation>Foil Notes</translation>
+    </message>
+    <message id="foilnotes-notification-copied_to_clipboard">
+        <source>Copied to clipboard</source>
+        <extracomment>Pop-up notification</extracomment>
+        <translation>Kopierat till urklipp</translation>
+    </message>
+    <message id="foilnotes-plaintext_view-placeholder">
+        <source>You do not have any notes.</source>
+        <extracomment>Placeholder text</extracomment>
+        <translation>Du har inga anteckningar.</translation>
+    </message>
+    <message id="foilnotes-encrypted_view-placeholder">
+        <source>You do not have any encrypted notes</source>
+        <extracomment>Placeholder text</extracomment>
+        <translation>Du har inga krypterade anteckningar.</translation>
+    </message>
+    <message id="foilnotes-plaintext_view-placeholder_hint">
+        <source>Open pulley menu to add one.</source>
+        <extracomment>Placeholder hint</extracomment>
+        <translation>Öppna toppmenyn för att lägga till en.</translation>
+    </message>
+    <message id="foilnotes-menu-new_note">
+        <source>New note</source>
+        <extracomment>Create a new note ready for editing</extracomment>
+        <translation>Ny anteckning</translation>
+    </message>
+    <message id="foilnotes-menu-new_secret_note">
+        <source>New secret note</source>
+        <extracomment>Create a new secret note ready for editing</extracomment>
+        <translation>Ny krypterad anteckning</translation>
+    </message>
+    <message id="foilnotes-menu-select_color">
+        <source>Select color</source>
+        <extracomment>Select note color</extracomment>
+        <translation>Välj färg</translation>
+    </message>
+    <message id="foilnotes-menu-delete_note">
+        <source>Delete note</source>
+        <extracomment>Delete this note from note page</extracomment>
+        <translation>Ta bort anteckningen</translation>
+    </message>
+    <message id="foilnotes-menu-encrypt">
+        <source>Encrypt</source>
+        <extracomment>Generic menu item</extracomment>
+        <translation>Kryptera</translation>
+    </message>
+    <message id="foilnotes-menu-decrypt">
+        <source>Decrypt</source>
+        <extracomment>Generic menu item</extracomment>
+        <translation>Dekryptera</translation>
+    </message>
+    <message id="foilnotes-menu-delete">
+        <source>Delete</source>
+        <extracomment>Generic menu item</extracomment>
+        <translation>Ta bort</translation>
+    </message>
+    <message id="foilnotes-menu-copy">
+        <source>Copy to clipboard</source>
+        <extracomment>Context menu item</extracomment>
+        <translation>Kopiera till urklipp</translation>
+    </message>
+    <message id="foilnotes-menu-change_password">
+        <source>Change password</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Ändra lösenord</translation>
+    </message>
+    <message id="foilnotes-remorse-deleting">
+        <source>Deleting</source>
+        <extracomment>Remorse item text, will delete note when timer expires</extracomment>
+        <translation>Tar bort</translation>
+    </message>
+    <message id="foilnotes-default_file_name">
+        <source>note</source>
+        <extracomment>Placeholder name for note filename</extracomment>
+        <translation>anteckning</translation>
+    </message>
+    <message id="foilnotes-placeholder-empty_note">
+        <source>Write a note...</source>
+        <extracomment>Placeholder text for new notes. At this point there is nothing else on the screen.</extracomment>
+        <translation>Skriv en anteckning...</translation>
+    </message>
+    <message id="foilnotes-menu-organize">
+        <source>Organize</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Organisera</translation>
+    </message>
+    <message id="foilnotes-menu-select">
+        <source>Select</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Välj</translation>
+    </message>
+    <message id="foilnotes-menu-select_all">
+        <source>Select all</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Markera alla</translation>
+    </message>
+    <message id="foilnotes-menu-select_none">
+        <source>Clear all</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Avmarkera alla</translation>
+    </message>
+    <message id="foilnotes-remorse-deleting_selected" numerus="yes">
+        <source>Deleting %0 selected notes</source>
+        <extracomment>Generic remorse popup text</extracomment>
+        <translation>
+            <numerusform>Tar bort %0 markerad anteckning</numerusform>
+            <numerusform>Tar bort %0 markerade anteckningar</numerusform>
+        </translation>
+    </message>
+    <message id="foilnotes-organize_page-plaintext_header">
+        <source>Organize notes</source>
+        <extracomment>Page header</extracomment>
+        <translation>Organisera anteckningar</translation>
+    </message>
+    <message id="foilnotes-organize_page-secret_header">
+        <source>Organize secret notes</source>
+        <extracomment>Page header</extracomment>
+        <translation>Organisera krypterade anteckningar</translation>
+    </message>
+    <message id="foilnotes-select_page-header">
+        <source>Select notes</source>
+        <extracomment>Page header</extracomment>
+        <translation>Välj anteckningar</translation>
+    </message>
+    <message id="foilnotes-notification-generated_key">
+        <source>Generated new key</source>
+        <extracomment>Pop-up notification</extracomment>
+        <translation>Genererade ny nyckel</translation>
+    </message>
+    <message id="foilnotes-notification-password_changed">
+        <source>Password changed</source>
+        <extracomment>Pop-up notification</extracomment>
+        <translation>Lösenord ändrat</translation>
+    </message>
+    <message id="foilnotes-enter_password_view-label-enter_password">
+        <source>Secret notes are locked. Please enter your password</source>
+        <extracomment>Password prompt label</extracomment>
+        <translation>Krypterade anteckningar är låsta. Ange ditt lösenord.</translation>
+    </message>
+    <message id="foilnotes-enter_password_view-button-unlocking">
+        <source>Unlocking...</source>
+        <extracomment>Button label</extracomment>
+        <translation>Låser upp...</translation>
+    </message>
+    <message id="foilnotes-enter_password_view-button-unlock">
+        <source>Unlock</source>
+        <extracomment>Button label</extracomment>
+        <translation>Lås upp</translation>
+    </message>
+    <message id="foilnotes-menu-generate_key">
+        <source>Generate a new key</source>
+        <extracomment>Pulley menu item</extracomment>
+        <translation>Generera en ny nyckel</translation>
+    </message>
+    <message id="foilnotes-decrypting_view-unlocking">
+        <source>Unlocking...</source>
+        <extracomment>Progress view label</extracomment>
+        <translation>Låser upp...</translation>
+    </message>
+    <message id="foilnotes-decrypting_view-decrypting">
+        <source>Decrypting...</source>
+        <extracomment>Progress view label</extracomment>
+        <translation>Avkrypterar...</translation>
+    </message>
+    <message id="foilnotes-generate_key_page-title">
+        <source>You are about to generate a new key</source>
+        <extracomment>Label text</extracomment>
+        <translation>Du håller på att generera en ny nyckel</translation>
+    </message>
+    <message id="foilnotes-generate_key_view-label-key_needed">
+        <source>You need to generate the key and select the password before you can encrypt your notes</source>
+        <extracomment>Label text</extracomment>
+        <translation>Du måste generera nyckeln och välja ett lösenord, innan du kan kryptera dina anteckningar.</translation>
+    </message>
+    <message id="foilnotes-generate_key_view-label-key_size">
+        <source>Key size</source>
+        <extracomment>Combo box label</extracomment>
+        <translation>Nyckelstorlek</translation>
+    </message>
+    <message id="foilnotes-generate_key_view-label-minimum_length" numerus="yes">
+        <source>Type at least %0 character(s)</source>
+        <extracomment>Password field label</extracomment>
+        <translation>
+            <numerusform>Mata in minst %0 tecken</numerusform>
+            <numerusform>Mata in minst %0 tecken</numerusform>
+        </translation>
+    </message>
+    <message id="foilnotes-generate_key_view-button-generate_key">
+        <source>Generate key</source>
+        <extracomment>Button label</extracomment>
+        <translation>Generera nyckel</translation>
+    </message>
+    <message id="foilnotes-generate_key_view-button-generating_key">
+        <source>Generating...</source>
+        <extracomment>Button label</extracomment>
+        <translation>Genererar...</translation>
+    </message>
+    <message id="foilnotes-generate_key_warning-title">
+        <source>Warning</source>
+        <extracomment>Title for the new key warning</extracomment>
+        <translation>Varning</translation>
+    </message>
+    <message id="foilnotes-generate_key_warning-text">
+        <source>You seem to have some encrypted files in the storage folder. Once you have generated a new key, you are going to lose access to those files. If you have forgotten your password, then keep in mind that most likely it&apos;s computationally easier to brute-force your password and recover the old key than to crack the new key.</source>
+        <extracomment>Warning shown prior to generating the new key</extracomment>
+        <translation>Du verkar ha några krypterade filer i lagringsmappen. Så fort du har genererat en ny nyckel, kommer du att förlora åtkomsten till dessa filer. Om du har glömt ditt lösenord, skall du veta att det sannolikt är beräkningsmässigt lättare att få fram (brute force) ditt lösenord och återställa den gamla nyckeln, än att knäcka den nya nyckeln.</translation>
+    </message>
+    <message id="foilnotes-generating_key_view-generating_new_key">
+        <source>Generating new key...</source>
+        <extracomment>Progress view label</extracomment>
+        <translation>Genererar ny nyckel...</translation>
+    </message>
+    <message id="foilnotes-change_password_page-label-enter_passwords">
+        <source>Please enter the current and the new password</source>
+        <extracomment>Password change prompt</extracomment>
+        <translation>Ange det nuvarande och det nya lösenordet</translation>
+    </message>
+    <message id="foilnotes-change_password_page-text_field_label-current_password">
+        <source>Current password</source>
+        <extracomment>Placeholder and label for the current password prompt</extracomment>
+        <translation>Aktuellt lösenord</translation>
+    </message>
+    <message id="foilnotes-change_password_page-text_field_label-new_password">
+        <source>New password</source>
+        <extracomment>Placeholder and label for the new password prompt</extracomment>
+        <translation>Nytt lösenord</translation>
+    </message>
+    <message id="foilnotes-change_password_page-button-change_password">
+        <source>Change password</source>
+        <extracomment>Button label</extracomment>
+        <translation>Ändra lösenord</translation>
+    </message>
+    <message id="foilnotes-confirm_password_page-info_label">
+        <source>Please type in your new password one more time</source>
+        <extracomment>Password confirmation label</extracomment>
+        <translation>Ange ditt nya lösenord en gång till</translation>
+    </message>
+    <message id="foilnotes-confirm_password_page-description">
+        <source>Make sure you don&apos;t forget your password. It&apos;s impossible to either recover it or to access the encrypted notes without knowing it. Better take it seriously.</source>
+        <extracomment>Password confirmation description</extracomment>
+        <translation>Tillse att du inte glömmer ditt lösenord. Det går inte att återställa lösenordet, eller att få tillgång till de krypterade anteckningarna utan det. Seriöst alltså!</translation>
+    </message>
+    <message id="foilnotes-confirm_password_page-text_field_placeholder-new_password">
+        <source>New password again</source>
+        <extracomment>Placeholder for the password confirmation prompt</extracomment>
+        <translation>Nytt lösenord igen</translation>
+    </message>
+    <message id="foilnotes-confirm_password_page-text_field_label-new_password">
+        <source>New password</source>
+        <extracomment>Label for the password confirmation prompt</extracomment>
+        <translation>Nytt lösenord</translation>
+    </message>
+    <message id="foilnotes-confirm_password_page-button-confirm">
+        <source>Confirm</source>
+        <extracomment>Button label (confirm password)</extracomment>
+        <translation>Bekräfta</translation>
+    </message>
+    <message id="foilnotes-remorse-decrypting">
+        <source>Decrypting</source>
+        <extracomment>Decrypting note in 5 seconds</extracomment>
+        <translation>Dekryptering</translation>
+    </message>
+    <message id="foilnotes-remorse-decrypting_selected" numerus="yes">
+        <source>Decrypting %0 selected notes</source>
+        <extracomment>Generic remorse popup text</extracomment>
+        <translation>
+            <numerusform>Avkrypterar %0 markerad anteckning</numerusform>
+            <numerusform>Avkrypterar %0 markerade anteckningar</numerusform>
+        </translation>
+    </message>
+    <message id="foilnotes-hint-delete_selected">
+        <source>Delete selected notes</source>
+        <extracomment>Hint text</extracomment>
+        <translation>Ta bort valda anteckningar</translation>
+    </message>
+    <message id="foilnotes-hint-encrypt_selected">
+        <source>Encrypt selected notes</source>
+        <extracomment>Hint text</extracomment>
+        <translation>Kryptera valda anteckningar</translation>
+    </message>
+    <message id="foilnotes-hint-decrypt_selected">
+        <source>Decrypt selected notes</source>
+        <extracomment>Hint text</extracomment>
+        <translation>Avkryptera valda anteckningar</translation>
+    </message>
+    <message id="foilnotes-foil_pics_warning">
+        <source>Note that Foil Notes and Foil Pics share the encryption key and the password.</source>
+        <extracomment>Warning text, small size label below the password prompt</extracomment>
+        <translation>Notera att Foil Notes och Foil Pics delar krypteringsnyckel och lösenord.</translation>
+    </message>
+    <message id="foilnotes-hint-swipe_left_to_plaintext">
+        <source>Swipe left to access plaintext notes</source>
+        <extracomment>Left swipe hint text</extracomment>
+        <translation>Svep åt vänster för oformaterade anteckningar</translation>
+    </message>
+    <message id="foilnotes-hint-swipe_right_to_encrypted">
+        <source>Encrypted pictures are moved there to the left</source>
+        <extracomment>Right swipe hint text</extracomment>
+        <translation>Krypterade anteckningar flyttas åt vänster</translation>
+    </message>
+    <message id="foilnotes-hint-swipe_left_to_decrypted">
+        <source>Decrypted notes are moved back to the right</source>
+        <extracomment>Left swipe hint text</extracomment>
+        <translation>Avkrypterade anteckningar flyttas tillbaka åt höger</translation>
+    </message>
+</context>
+</TS>

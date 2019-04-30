f8fc32124a0f6deed84973643e84b2708bdd87a7
Åke Engelbrektson <eson57@users.noreply.github.com>
2019-04-30 06:26:08 +0200

    Update harbour-foilnotes-sv.ts
    Fix changed string

--- translations/harbour-foilnotes-sv.ts
+++ translations/harbour-foilnotes-sv.ts
@@ -41,7 +41,7 @@
     <message id="foilnotes-menu-show_qrcode">
         <source>Show QR code</source>
         <extracomment>Show QR code for the current note</extracomment>
-        <translation type="unfinished">Visa QR-kod</translation>
+        <translation>Visa QR-kod för aktuell anteckning</translation>
     </message>
     <message id="foilnotes-menu-select_color">
         <source>Select color</source>

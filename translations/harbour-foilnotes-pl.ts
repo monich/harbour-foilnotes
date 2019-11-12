a3d7cdae9e9846d2427d70dfa95d256a595d1efb
A <hucul@o2.pl>
2019-11-12 19:38:32 +0100

    Update harbour-foilnotes-pl.ts

--- translations/harbour-foilnotes-pl.ts
+++ translations/harbour-foilnotes-pl.ts
@@ -41,7 +41,7 @@
     <message id="foilnotes-menu-show_qrcode">
         <source>Show QR code</source>
         <extracomment>Show QR code for the current note</extracomment>
-        <translation>Pokaż kod QR dla bieżącej notatki</translation>
+        <translation>Pokaż kod QR</translation>
     </message>
     <message id="foilnotes-menu-select_color">
         <source>Select color</source>

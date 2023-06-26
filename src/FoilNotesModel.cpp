28e21ce116f995c7040554e5a7dc059a3ef998d4
Slava Monich <slava@monich.com>
2023-06-27 02:17:01 +0300

    [foilnotes] Set 0600 file mode for encrypted files
    Not that it's overly important, it's just strange to see encrypted files
    with 0666 permissions.

--- src/FoilNotesModel.cpp
+++ src/FoilNotesModel.cpp
@@ -1,6 +1,6 @@
 /*
+ * Copyright (C) 2018-2023 Slava Monich <slava@monich.com>
  * Copyright (C) 2018-2021 Jolla Ltd.
- * Copyright (C) 2018-2021 Slava Monich <slava@monich.com>
  *
  * You may use this file under the terms of the BSD license as follows:
  *
@@ -8,27 +8,29 @@
  * modification, are permitted provided that the following conditions
  * are met:
  *
- *   1. Redistributions of source code must retain the above copyright
- *      notice, this list of conditions and the following disclaimer.
- *   2. Redistributions in binary form must reproduce the above copyright
- *      notice, this list of conditions and the following disclaimer in
- *      the documentation and/or other materials provided with the
- *      distribution.
- *   3. Neither the names of the copyright holders nor the names of its
- *      contributors may be used to endorse or promote products derived
- *      from this software without specific prior written permission.
+ *  1. Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *  2. Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer
+ *     in the documentation and/or other materials provided with the
+ *     distribution.
+ *  3. Neither the names of the copyright holders nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) ARISING
+ * IN ANY WAY OUT OF THE USE OR INABILITY TO USE THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * The views and conclusions contained in the software and documentation
+ * are those of the authors and should not be interpreted as representing
+ * any official policies, either expressed or implied.
  */
 
 #include "FoilNotesModel.h"
@@ -45,12 +47,16 @@
 #include "foilmsg.h"
 
 #include <unistd.h>
+#include <string.h>
+#include <errno.h>
 #include <sys/stat.h>
 
 #define ENCRYPT_KEY_TYPE        FOILMSG_KEY_AES_256
 #define SIGNATURE_TYPE          FOILMSG_SIGNATURE_SHA256_RSA
 
 #define HEADER_COLOR            "color"
+
+#define ENCRYPT_FILE_MODE       0600
 
 // Directories relative to home
 #define FOIL_NOTES_DIR          "Documents/FoilNotes"
@@ -292,6 +298,9 @@ void FoilNotesModel::ModelInfo::save(QString aDir, FoilPrivateKey* aPrivate,
         foilmsg_encrypt(out, &data, NULL, &headers, aPrivate, aPublic,
             &opt, NULL);
         foil_output_unref(out);
+        if (chmod(fname, ENCRYPT_FILE_MODE) < 0) {
+            HWARN("Failed to chmod" << fname << strerror(errno));
+        }
     } else {
         HWARN("Failed to open" << fname);
     }
@@ -576,6 +585,9 @@ void FoilNotesModel::EncryptTask::performTask()
             QString path(QString::fromLocal8Bit(dest->str, dest->len));
             iData = new ModelData(iBody, iColor, 0, path, iNoteId);
             removeFile(iRemoveFile);
+            if (chmod(dest->str, ENCRYPT_FILE_MODE) < 0) {
+                HWARN("Failed to chmod" << dest->str << strerror(errno));
+            }
         }
 
         foil_output_unref(out);
